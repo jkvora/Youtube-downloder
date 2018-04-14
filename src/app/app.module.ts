6d845a906f30ccb141d25cff09f20e75be43ceda
Jalak <vorajalak@yahoo.co.in>
2018-04-14 19:35:49 +0530

    Initial  Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import {MatButtonModule, MatCheckboxModule,MatToolbarModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [    
+    AppComponent,
+  
   ],
   imports: [
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
     BrowserModule
+    
   ],
   providers: [],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
